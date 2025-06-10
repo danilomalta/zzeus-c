575640fd1acadb97ecb87db85250aea1a8aed61b
danilomalta <danilorsmalta@gmail.com>
2025-06-10 15:18:03 -0300

    Delete zzeus-frontend-completo-final directory

--- zzeus-frontend-completo-final/components/menuLateral.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuHamburger = document.querySelector('.menu-hamburger');
-    const sidebar = document.querySelector('.sidebar');
-    menuHamburger.addEventListener('click', () => {
-        sidebar.classList.toggle('active');
-    });
-});
